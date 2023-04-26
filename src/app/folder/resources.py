c4cf39778b9e9362d8216c3a9adf2b470b3c7a65
ivan-tana <84873112+ivan-tana@users.noreply.github.com>
2023-04-26 20:09:36 +0100

    added documentation fro folder resource

--- src/app/folder/resources.py
+++ src/app/folder/resources.py
@@ -6,6 +6,24 @@ from app.extensions import database
 
 class Folder(Resource):
     def get(self):
+        """
+        This method queries all the folders in the database and returns a dictionary with their information.
+
+        Parameters:
+        - self: the instance of the class that calls this method
+
+        Returns:
+        - a dictionary with two keys: 'count' and 'folders'
+        - 'count' is an integer that represents the number of folders in the database
+        - 'folders' is a list of dictionaries, each containing the attributes of a folder instance
+        - a folder instance has the following attributes:
+            - 'name': a string that is the name of the folder
+            - 'files': a list of dictionaries, each containing the attributes of a file instance that belongs to the folder
+            - 'all_files': a list of dictionaries, each containing the attributes of a file instance that belongs to the folder or any of its subfolders
+            - 'folders': a list of dictionaries, each containing the attributes of a subfolder instance that belongs to the folder
+            - 'allowed_extension': a list of strings that are the allowed file extensions for the folder
+        """
+        # query all the folders in the database
         folders = FolderM.query.all()
 
         context = {
@@ -15,16 +33,33 @@ class Folder(Resource):
         return context
 
     def post(self):
+        """
+        This method creates a new folder in the database and returns the names of the files in it.
+
+        Parameters:
+        - self: the instance of the class that calls this method
+        - path: a string argument that specifies the path of the folder to be created
+
+        Returns:
+        - a dictionary with a key 'path' and a value that is a list of file names in the folder
+        """
+        # create a parser object to parse the path argument
         folder_pars = reqparse.RequestParser()
         folder_pars.add_argument('path')
         args = folder_pars.parse_args()
 
+        # convert the path argument to a Path object
         path = Path(args['path'])
+        # check if the path exists
         if path.exists():
+            # create a FolderM object with the path as an attribute
             new_folder = FolderM(path=str(path))
+            # add the folder to the database session
             database.session.add(new_folder)
+            # commit the changes to the database
             database.session.commit()
 
+            # return a dictionary with the file names in the folder
             return {
                 'path': [file.name for file in new_folder.all_files]
             }
